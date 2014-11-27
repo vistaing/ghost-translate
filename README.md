36547b4f56dd40654b0fa4ce36ad31a9508441b6
Clément Osternaud <osternaud.clement@gmail.com>
2014-11-27 10:48:43 +0100

    Add minify version

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
+## About Ghost-Translate
+**Ghost Translate** is a short jquery plugin to add several language into yout Ghost posts.
+
+## Instalation
+Clone or download this repository into your Ghost themes and add the plugin into your *default.hbs* template.
+
+    <script src="{{asset "lib/ghost-translate/ghost-translate.js"}}"></script>
+
+To initialise the plugin, past this code into your main javascript file:
+
+``$('.post-content').ghostTranslate();``
+
+**.post-content** is the post container find into *post.hbs* template.
+
+You can change some parameters:
+
+### Add language
+Define your language into the "lang" options:
+
+    "lang": [
+        {
+            "name": "Anglais",
+            "short": "en"
+        },
+        {
+            "name": "Français",
+            "short": "fr"
+        }
+    ]
+    
+### Selector type
+Actualy, the plugin can create two type of selector: **List** or **Select**.
+
+To choose, change the "type_selector" option:
+``"type_selector": "list" // or "select"``
+
+## In your post
+Now the plugin is installed and initialized.
+In your Ghost post, separate each language with this:
+    
+    == start en ==
+        <p>Your content</p>
+    == end end ==
+    
+    == start fr ==
+        <p>Votre contenu</p>
+    == end fr ==
+
+
 # TO-DO
 
 - add jquery.cookie
-- add selector languages (select or ul > li)
---
+++ ghost-translate.js
@@ -0,0 +1,98 @@
+/**
+*   Name: Ghist Translate Plugin
+*   By: @osternaud_clem
+*/
+
+(function($){ 
+    $.fn.ghostTranslate=function(options){
+
+        var defauts = {
+            "lang": [
+                {
+                    "name": "Anglais",
+                    "short": "en"
+                },
+                {
+                    "name": "Français",
+                    "short": "fr"
+                }
+            ],
+            "type_selector": "list",            
+            "container": null,
+            "cookie": false
+        };  
+
+        var p = $.extend(defauts, options); 
+
+        return this.each(function(){	
+            var that = $(this);
+            var defauts = p.lang[0].short;
+
+            var allContent = $(this).html();
+            
+            $(this).html('');		
+
+            create_select(p.lang, p.type_selector, $(this));	
+
+            $('.ghost-select-lang').after('<div class="ghost-translate-content"></div>');
+
+            $('.ghost-translate-content').html(get_translate(defauts, allContent));
+
+            change_lang(p.type_selector, allContent);
+
+        });
+        
+        
+        /**
+            Get the translation content 
+        **/
+        
+        function get_translate(lang, content){
+            var regExp = new RegExp("== start "+lang+" ==([^]+)== end "+lang+" ==");
+
+            var matches = regExp.exec(content);
+
+            return matches[1];
+        }
+        
+        /**
+            Create lang selector (List or Select)
+        **/
+        function create_select(langs, type, container){
+
+            if(type == 'list'){
+                container.prepend('<ul class="ghost-select-lang"></ul>');
+
+                $.each(langs, function(val){
+                    lang  = langs[val];
+                    $('.ghost-select-lang').append('<li class="lang-item" data-lang="'+lang.short+'">'+lang.name+'</li>');
+                });
+            }else{
+                container.prepend('<select class="ghost-select-lang"></select>');
+
+                $.each(langs, function(val){
+                    lang  = langs[val];
+                    $('.ghost-select-lang').append('<option value="'+lang.short+'">'+lang.name+'</option>');
+                });
+            }                    
+        }
+        
+        /**
+            Change content language by cliking or changing the selector
+        **/
+        function change_lang(type, content){
+            if(type == 'list'){                
+                $('.ghost-select-lang .lang-item').click(function(){
+                    var lang = $(this).data('lang');
+                    $('.ghost-translate-content').html(get_translate(lang, content));
+                });
+            }else{
+                $('.ghost-select-lang').change(function(){
+                    var lang = $(this).val();
+                    $('.ghost-translate-content').html(get_translate(lang, content));
+
+                });
+            }
+        }
+    };
+})(jQuery);
